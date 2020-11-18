e666bf9277cb8b4c8cf51d130cb766125ea200ae
hamzahasn <hamza.hasnain@gmail.com>
2020-11-18 02:10:08 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -6,5 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our", "a", "thee", "your"];
+  var adj = ["scary", "terrifying", "horrific", "frightening", "spooky"];
+  var noun = ["dracula", "frankenstein", "werewolf", "ghost", "mummy", "ghoul"];
+
+  var domain = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let newDomain = pronoun[i] + adj[j] + noun[k] + ".com";
+        domain.push(newDomain);
+      }
+    }
+  }
+  console.log(domain);
+  let ul = document.querySelector("#domain-list");
+  ul.innerHTML = buildList(domain);
 };
+
+function buildList(list) {
+  return list
+    .map(item => {
+      return "<li>" + item + "</li>";
+    })
+    .join("");
+}
